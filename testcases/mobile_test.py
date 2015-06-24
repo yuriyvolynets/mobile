77d413f495cc4aef8a0f198a7729889a27508a93
yuriyvolynets <yuriy.volynets@axilera.com>
2015-06-24 11:26:38 +0300

    Created repository with test cases.

---
+++ testcases/mobile_test.py
@@ -0,0 +1,8 @@
+# !/usr/bin/python
+
+class TestMobileApplication(object):
+    def test_1(self, mobile_app):
+        mobile_app.stub_1()
+
+    def test_2(self, mobile_app):
+        mobile_app.stub_2()
